e6df4754f2db140e3439f5c194aff8d87d6c9c37
John Ponce <johnponce@Johns-MacBook-Air.local>
2024-10-07 12:06:30 +0800

    adding variables.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Optional
+}
+resource "aws_instance" "instance" {
+  ami           = "aws_ami.ubuntu.id"
+  name          = var.instance_name
+  instance_type = var.instance_type
+}
